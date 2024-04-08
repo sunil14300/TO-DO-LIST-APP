056cc755cd3c84b2719a9bc07fe42e7569bc5fc3
sunil kumar <114928585+sunil14300@users.noreply.github.com>
2024-04-09 00:43:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputBox=document.getElementById('input-box');
+
+const insertContainer=document.getElementById('list-container');
+
+
+function AddTask(){
+    if(inputBox.value==''){
+        alert("you must write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        insertContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData()
+}
+
+insertContainer.addEventListener("click",function(e){
+    if(e.target.tagName=="LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+
+},false)
+
+function saveData(){
+    localStorage.setItem("data",insertContainer.innerHTML);
+}
+
+function showTask(){
+    insertContainer.innerHTML=localStorage.getItem("data")
+}
+
+showTask()
